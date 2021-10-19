549db9e070e170fa6aa6b0c5f1f8f58f9280fa7c
Jerome Amon <aaboujerome@gmail.com>
2021-10-19 17:45:11 +0200

    refactored & added Breadth-First Search (BFS) implementation into navigate-search-into-non-binary-trees.go

--- navigate-search-into-non-binary-trees.go
+++ navigate-search-into-non-binary-trees.go
@@ -9,34 +9,18 @@ type Node struct {
 	childs []*Node
 }
 
-// locateMe uses Depth-First Search (DFS) to
-// check if a given node-value is present.
-func locateMe(node *Node, me string) bool {
-	fmt.Println("=>", node.value)
-	if node.value == me {
-		fmt.Println("=> found me.")
-		return true
-	}
+var Root *Node
 
-	// recursive on childs nodes.
-	if len(node.childs) > 0 {
-		for _, child := range node.childs {
-			if locateMe(child, me) {
-				// found so stop here.
-				return true
-			}
-		}
-	}
-	// not found.
-	return false
+func init() {
+	BuildTree()
 }
 
-func main() {
-
-	// Leaf on branch A
-	LA := Node{"LA", []*Node{}}
+// BuildTree constructs default Tree.
+func BuildTree() {
+	// First Leaf on branch A
+	L0A := Node{"L0A", []*Node{}}
 	// Fourth level node on branch A
-	A4 := Node{"A4", []*Node{&LA}}
+	A4 := Node{"A4", []*Node{&L0A}}
 	// Third level node on branch A
 	A3 := Node{"A3", []*Node{&A4}}
 	// Second level node on branch A
@@ -44,10 +28,24 @@ func main() {
 	// First level node on branch A
 	A1 := Node{"A1", []*Node{&A2}}
 
-	// Leaf on branch B
-	LB := Node{"LB", []*Node{}}
+	// Second Leaf on branch A
+	L1A := Node{"L1A", []*Node{}}
+	// Fourth level node on branch A
+	A44 := Node{"A44", []*Node{&L1A}}
+	// Third level node on branch A
+	A33 := Node{"A33", []*Node{&A44}}
+	// Second level node on branch A
+	A22 := Node{"A22", []*Node{&A33}}
+	// First level node on branch A
+	A11 := Node{"A11", []*Node{&A22}}
+
+	// Root level node of branch A
+	A0 := Node{"A0", []*Node{&A1, &A11}}
+
+	// First Leaf on branch B
+	L0B := Node{"L0B", []*Node{}}
 	// Fourth level node on branch B
-	B4 := Node{"B4", []*Node{&LB}}
+	B4 := Node{"B4", []*Node{&L0B}}
 	// Third level node on branch B
 	B3 := Node{"B3", []*Node{&B4}}
 	// Second level node on branch B
@@ -55,51 +53,182 @@ func main() {
 	// First level node on branch B
 	B1 := Node{"B1", []*Node{&B2}}
 
-	// Leaf on branch C
-	LC := Node{"LC", []*Node{}}
+	// Second Leaf on branch B
+	L1B := Node{"L1B", []*Node{}}
+	// Fourth level node on branch B
+	B44 := Node{"B44", []*Node{&L1B}}
+	// Third level node on branch B
+	B33 := Node{"B33", []*Node{&B44}}
+	// Second level node on branch B
+	B22 := Node{"B22", []*Node{&B33}}
+	// First level node on branch B
+	B11 := Node{"B11", []*Node{&B22}}
+
+	// Root level node of branch B
+	B0 := Node{"B0", []*Node{&B1, &B11}}
+
+	// First Leaf on branch C
+	L0C := Node{"L0C", []*Node{}}
 	// Fourth level node on branch C
-	C4 := Node{"C4", []*Node{&LC}}
+	C4 := Node{"C4", []*Node{&L0C}}
 	// Third level node on branch C
-	C3 := Node{value:"C3", childs:[]*Node{&C4}}
+	C3 := Node{"C3", []*Node{&C4}}
 	// Second level node on branch C
 	C2 := Node{"C2", []*Node{&C3}}
 	// First level node on branch C
 	C1 := Node{"C1", []*Node{&C2}}
 
-	// Leaf on branch D
-	LD := Node{"LD", []*Node{}}
+	// Second Leaf on branch C
+	L1C := Node{"L1C", []*Node{}}
+	// Fourth level node on branch C
+	C44 := Node{"C44", []*Node{&L1C}}
+	// Third level node on branch C
+	C33 := Node{"C33", []*Node{&C44}}
+	// Second level node on branch C
+	C22 := Node{"C22", []*Node{&C33}}
+	// First level node on branch C
+	C11 := Node{"C11", []*Node{&C22}}
+
+	// Root level node of branch C
+	C0 := Node{"C0", []*Node{&C1, &C11}}
+
+	// First Leaf on branch D
+	L0D := Node{"L0D", []*Node{}}
 	// Fourth level node on branch D
-	D4 := Node{"D4", []*Node{&LD}}
+	D4 := Node{"D4", []*Node{&L0D}}
 	// Third level node on branch D
-	D3 := Node{value:"D3", childs:[]*Node{&D4}}
+	D3 := Node{"D3", []*Node{&D4}}
 	// Second level node on branch D
 	D2 := Node{"D2", []*Node{&D3}}
 	// First level node on branch D
 	D1 := Node{"D1", []*Node{&D2}}
 
-	// Root of the Tree.
-	R := Node{"R", []*Node{&A1, &B1, &C1, &D1}}
+	// Second Leaf on branch D
+	L1D := Node{"L1D", []*Node{}}
+	// Fourth level node on branch D
+	D44 := Node{"D44", []*Node{&L1D}}
+	// Third level node on branch D
+	D33 := Node{"D33", []*Node{&D44}}
+	// Second level node on branch D
+	D22 := Node{"D22", []*Node{&D33}}
+	// First level node on branch D
+	D11 := Node{"D11", []*Node{&D22}}
 
-	_ = locateMe(&R, "C3")
+	// Root level node of branch D
+	D0 := Node{"D0", []*Node{&D1, &D11}}
+
+	// Root of the Tree.
+	Root = &Node{"R0", []*Node{&A0, &B0, &C0, &D0}}
 }
 
-/*
-[ 0:06:47] {nxos-geek}:~$ go run navigate-into-tree.go
+// LocateMe uses Depth-First Search (DFS) to
+// check if a given node-value is present.
+// It traverses the tree depth.
+func LocateMeByDFS(node *Node, me string) bool {
+	fmt.Printf("=> %s\t", node.value)
+	if len(node.childs) == 0 {
+		// next branch so move to next line.
+		fmt.Println()
+	}
 
-=> R
+	if node.value == me {
+		fmt.Println("\n\n:: found me by DFS.")
+		return true
+	}
+
+	// recursive on childs nodes.
+	if len(node.childs) > 0 {
+		for _, child := range node.childs {
+			if LocateMeByDFS(child, me) {
+				// found so stop here.
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// LocateMeByBFS uses Breadth-First Search (BFS) to
+// check if a given node-value is present.
+// It traverses the tree width.
+func LocateMeByBFS(node *Node, me string) bool {
+	// slice to store nodes for processing.
+	nodesToProcess := []*Node{node}
+	// until there is a node - check.
+	for len(nodesToProcess) > 0 {
+		// pick up the first node.
+		currentNode := nodesToProcess[0]
+		// remove that node from the list.
+		nodesToProcess = nodesToProcess[1:]
+		fmt.Printf("=> %s\n", currentNode.value)
+		if currentNode.value == me {
+			// found me.
+			fmt.Println("\n:: found me by BFS.")
+			return true
+		}
+
+		// me not found, so add & check currentNode childs.
+		if len(currentNode.childs) > 0 {
+			for _, child := range currentNode.childs {
+				nodesToProcess = append(nodesToProcess, child)
+			}
+		}
+	}
+	return false
+}
+
+func main() {
+	if LocateMeByDFS(Root, "C3") == false {
+		fmt.Println("\n\n:: not found me by DFS.")
+	}
+
+	fmt.Println("\n")
+
+	if LocateMeByBFS(Root, "C3") == false {
+		fmt.Println("\n\n:: not found me by BFS.")
+	}
+}
+
+/* Output:
+
+{nxos-geek}:~$ go run navigate-search-into-non-binary-trees.go
+=> R0   => A0   => A1   => A2   => A3   => A4   => L0A
+=> A11  => A22  => A33  => A44  => L1A
+=> B0   => B1   => B2   => B3   => B4   => L0B
+=> B11  => B22  => B33  => B44  => L1B
+=> C0   => C1   => C2   => C3
+
+:: found me by DFS.
+
+
+=> R0
+=> A0
+=> B0
+=> C0
+=> D0
 => A1
-=> A2
-=> A3
-=> A4
-=> LA
+=> A11
 => B1
-=> B2
-=> B3
-=> B4
-=> LB
+=> B11
 => C1
+=> C11
+=> D1
+=> D11
+=> A2
+=> A22
+=> B2
+=> B22
 => C2
+=> C22
+=> D2
+=> D22
+=> A3
+=> A33
+=> B3
+=> B33
 => C3
-=> found me.
+
+:: found me by BFS.
 
 */
