2f283d35c8306212d7cc03832f29b8ce8184acc8
Jerome Amon <aaboujerome@gmail.com>
2025-05-27 23:23:22 +0200

    add challenge notes for move-list-items repo

---
+++ move-list-items/main.go
@@ -0,0 +1,5 @@
+package main
+
+// list of integers and shift them left by a given number of places n
+// looping around to the end of the list.
+// given the input: [1,2,3,4,5,6,7,8] n = 3 => output: [4,5,6,7,8,1,2,3]
