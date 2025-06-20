e76382cb2bb780303b0408572a32ae73714a1753
Jerome Amon <aaboujerome@gmail.com>
2025-06-20 21:27:17 +0000

    add move to right routine into move-list-items

--- move-list-items/main.go
+++ move-list-items/main.go
@@ -16,7 +16,33 @@ func MoveToLeft(input []int, n int) []int {
 		if newIndex := currentIndex - n; newIndex >= 0 {
 			output[newIndex] = value
 		} else {
-			newIndex = lg + newIndex
+			newIndex = newIndex + lg
+			if newIndex < 0 {
+				newIndex *= -1
+			}
+			output[newIndex] = value
+		}
+	}
+	return output
+}
+
+// list of integers and shift them right by a given number of places n
+// looping around to the end of the list.
+// given the input: [1,2,3,4,5,6,7,8] n = 3 => output: [6,7,8,1,2,3,4,5]
+
+func MoveToRight(input []int, n int) []int {
+	lg := len(input)
+	if lg <= 1 {
+		return input
+	}
+
+	output := make([]int, lg)
+
+	for currentIndex, value := range input {
+		if newIndex := currentIndex + n; newIndex < lg {
+			output[newIndex] = value
+		} else {
+			newIndex = newIndex % lg
 			output[newIndex] = value
 		}
 	}
--- move-list-items/main_test.go
+++ move-list-items/main_test.go
@@ -28,6 +28,11 @@ func TestMoveToLeft(t *testing.T) {
 			want:   []int{1},
 		},
 		{
+			input:  []int{1, 2},
+			places: 3,
+			want:   []int{2, 1},
+		},
+		{
 			input:  []int{},
 			places: 4,
 			want:   []int{},
@@ -42,6 +47,54 @@ func TestMoveToLeft(t *testing.T) {
 	for n, tc := range cases {
 		t.Run(fmt.Sprint("case:", n+1), func(t *testing.T) {
 			got := MoveToLeft(tc.input, tc.places)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("got: %v but want: %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestMoveToRight(t *testing.T) {
+	cases := []struct {
+		input  []int
+		places int
+		want   []int
+	}{
+		{
+			input:  []int{1, 2, 3, 4, 5, 6, 7, 8},
+			places: 3,
+			want:   []int{6, 7, 8, 1, 2, 3, 4, 5},
+		},
+		{
+			input:  []int{6, 7, 8, 1, 2, 3, 2, 5},
+			places: 3,
+			want:   []int{3, 2, 5, 6, 7, 8, 1, 2},
+		},
+		{
+			input:  []int{1},
+			places: 3,
+			want:   []int{1},
+		},
+		{
+			input:  []int{1, 2},
+			places: 3,
+			want:   []int{2, 1},
+		},
+		{
+			input:  []int{},
+			places: 4,
+			want:   []int{},
+		},
+		{
+			input:  nil,
+			places: 4,
+			want:   nil,
+		},
+	}
+
+	for n, tc := range cases {
+		t.Run(fmt.Sprint("case:", n+1), func(t *testing.T) {
+			got := MoveToRight(tc.input, tc.places)
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("got: %v but want: %v", got, tc.want)
 			}
