33e122f1e66eb4d4983671d601d2f97131c2129b
Jerome Amon <aaboujerome@gmail.com>
2025-06-20 20:46:45 +0000

    add unit test for move-list-items

--- move-list-items/main.go
+++ move-list-items/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // list of integers and shift them left by a given number of places n
 // looping around to the end of the list.
 // given the input: [1,2,3,4,5,6,7,8] n = 3 => output: [4,5,6,7,8,1,2,3]
@@ -23,23 +21,4 @@ func MoveToLeft(input []int, n int) []int {
 		}
 	}
 	return output
-}
-
-func main() {
-	input := []int{1, 2, 3, 4, 5, 6, 7, 8}
-	n := 3
-	output := MoveToLeft(input, n)
-	fmt.Println("output:", output)
-
-	input = []int{1}
-	output = MoveToLeft(input, n)
-	fmt.Println("output:", output)
-
-	input = []int{}
-	output = MoveToLeft(input, n)
-	fmt.Println("output:", output)
-
-	input = nil
-	output = MoveToLeft(input, n)
-	fmt.Println("output:", output)
 }
---
+++ move-list-items/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestMoveToLeft(t *testing.T) {
+	cases := []struct {
+		input  []int
+		places int
+		want   []int
+	}{
+		{
+			input:  []int{1, 2, 3, 4, 5, 6, 7, 8},
+			places: 3,
+			want:   []int{4, 5, 6, 7, 8, 1, 2, 3},
+		},
+		{
+			input:  []int{4, 5, 6, 7, 8, 1, 2, 3},
+			places: 3,
+			want:   []int{7, 8, 1, 2, 3, 4, 5, 6},
+		},
+		{
+			input:  []int{1},
+			places: 3,
+			want:   []int{1},
+		},
+		{
+			input:  []int{},
+			places: 4,
+			want:   []int{},
+		},
+		{
+			input:  nil,
+			places: 4,
+			want:   nil,
+		},
+	}
+
+	for n, tc := range cases {
+		t.Run(fmt.Sprint("case:", n+1), func(t *testing.T) {
+			got := MoveToLeft(tc.input, tc.places)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("got: %v but want: %v", got, tc.want)
+			}
+		})
+	}
+}
